66f44017dbcec04b8463b841ae8b9f8599b92be1
beebooplena <lena_kath@hotmail.com>
2021-12-07 17:04:04 +0000

    #points new left update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const playerResult = document.getElementById('player-one');
 const computerResult = document.getElementById('player-two');
 const computerChoice = document.getElementById('computer-player');
-const playerChoice = document.getElementById('play-screen');
+const playerChoice = document.getElementById('player');
 const resetBtn = document.getElementById('reset');
 const imageComputer = document.getElementById('computer-image');
 const imagePlayer = document.getElementById('player-image');
@@ -19,11 +19,24 @@ document.addEventListener("DOMContentLoaded",function() {
 function runGame() {
     generateChoice()
     compareChoice()
+    computerChoice.innerHTML = '<img src = "' + computerChoice + '.jpg">';
+    gameResults()
 }
 
-function generateComputer() {
+function generateChoice() {
     ranNum = Math.floor(Math.random() * 2) + 1;
     if (ranNum == 0){ computerChoice = "data-choice = 0";}
     else if(ranNum == 1){computerChoice = "data-choice = 1";}
     else if(ranNum == 2){computerChoice = " data-choice = 2";}
+}
+
+function compareChoice() {
+    if (playerChoice === computerChoice){playerResult, computerResult = "It`s a draw!";}
+    else if (playerChoice == "data-choice = 0" && computerChoice == "data-choice = 2"){playerResult = "You Win!";}
+    else if(playerChoice == "data-choice = 1" && computerChoice == "data-choice = 0"){playerResult = "You Win!";}
+    else if(playerChoice == "data-choice = 2" && computerChoice == "data-choice = 1"){playerResult = "You Win!";}
+    else if(playerChoice == "data-choice = 2" && computerChoice == "data-choice = 0"){playerResult = "You loose";}
+    else if(playerChoice == "data-choice = 0" && computerChoice == "data-choice = 1"){playerResult = "You loose!";}
+    else if(playerChoice == "data-choice = 1" && computerChoice == "data-choice = 2"){playerResult = "You loose!";}
+
 }
