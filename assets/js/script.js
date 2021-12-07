cc6bed3d789dc59a288c592160b0becb7ee3d08f
beebooplena <lena_kath@hotmail.com>
2021-12-07 14:15:43 +0000

    added starting images to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const playerResult = document.getElementById('player-one');
+const computerResult = document.getElementById('player-two');
+const computerChoice = document.getElementById('computer-player');
+const playerChoice = document.getElementById('play-screen');
+const resetBtn = document.getElementById('reset');
+const imageComputer = document.getElementById('computer-image');
+const imagePlayer = document.getElementById('player-image');
+const choices = ["rock", "paper", "scissors"];
+
+function runGame()
+function generateChoice()
+
+function compareChoices()
+
+function incrementPlayerScore()
+function incrementComputerScore()
+function reset()
