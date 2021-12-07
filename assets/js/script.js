594073fa97f66c2cbc5ac18bada7ba987ac59e0a
beebooplena <lena_kath@hotmail.com>
2021-12-07 16:33:22 +0000

    wrote compareChoice() with different compares

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,22 @@ const imagePlayer = document.getElementById('player-image');
 const choices = ["rock", "paper", "scissors"];
 
 
+document.addEventListener("DOMContentLoaded",function() {
+    let buttons = document.getElementsByTagName("button");
+    button.addEventListener("click", function(){
+        if (this.getAttribute( "data-choice") == 0)
+        {runGame();}else if(this.getAttribute( "data-choice") == 1)
+        {runGame();}else(this.getAttribute( "data-choice") == 2)
+        {runGame();}
+
+function runGame() {
+    generateChoice()
+    compareChoice()
+}
+
+function generateComputer() {
+    ranNum = Math.floor(Math.random() * 2) + 1;
+    if (ranNum == 0){ computerChoice = "data-choice = 0";}
+    else if(ranNum == 1){computerChoice = "data-choice = 1";}
+    else if(ranNum == 2){computerChoice = " data-choice = 2";}
+}
