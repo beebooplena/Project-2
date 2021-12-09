0931285459ac8ae03a3942ceb6ea6a3395af55bf
beebooplena <lena_kath@hotmail.com>
2021-12-09 01:52:45 +0000

    added else if computerChoice()

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,35 +48,74 @@ function playerScore() {
 
     let playerResult = parseInt(document.getElementById("player-one").innerText);
     document.getElementById("player-one").innerText = ++playerResult;
-   }
+}
+console.log("iiii");
 
-   function computerScore() {
+function computerScore() {
     let computerResult = parseInt(document.getElementById("player-two").innerText);
- document.getElementById("player-two").innerText = ++computerResult;
-console.log(iiii);
+    document.getElementById("player-two").innerText = ++computerResult;
+
 }
 
-    function generateComputerChoice() {
-        console.log("hei")
-        let ranNum = Math.floor(Math.random() * 3);
-        if  (ranNum == 0) {
-            divComputerImage.style.background = "url(/assets/images/paper.jpg)";
-        } else if (ranNum === 1) {
-            divComputerImage.style.background = "url(/assets/images/scissors.jpg)";
-        } else if (ranNum === 2) {
-            divComputerImage.style.background = "url(/assets/images/stone.jpg)";
-        }
-        return ranNum
-
-
-
+function generateComputerChoice() {
+    console.log("hei")
+    let ranNum = Math.floor(Math.random() * 3);
+    if (ranNum == 0) {
+        divComputerImage.style.background = "url(/assets/images/paper.jpg)";
+    } else if (ranNum === 1) {
+        divComputerImage.style.background = "url(/assets/images/scissors.jpg)";
+    } else if (ranNum === 2) {
+        divComputerImage.style.background = "url(/assets/images/stone.jpg)";
     }
+    return ranNum
 
-    function compareChoice(computerChoice, playerChoice) { 
-        if (playerChoice == computerChoice){playerResult ="It`s a draw";
-      } else if(playerChoice == 1 && computerChoice == 0){playerResult = "You Win!";
-    playerScore()}
+
+
+}
+
+function compareChoice(computerChoice, decisionChoice) {
+    if (decisionChoice == computerChoice) {
+       
+    } else if (decisionChoice == 1 && computerChoice == 0) {
         
+        playerScore()
+    } else if (decisionChoice == 0 && computerChoice == 2) {
+        
+        playerScore()
+    } else if (decisionChoice == 2 && computerChoice == 1) {
+       
+        playerScore()
+    } else if (decisionChoice == 2 && computerChoice == 0) {
+       
+        computerScore()
+    } else if (decisionChoice == 0 && computerChoice == 1) {
+        
+        computerScore()
+    } else if (decisionChoice == 1 && computerChoice == 2) 
+    computerScore()
 
-    }
+console.log("aaa");
+if (decisionChoice == computerChoice) {
+    alert("It`s a draw");
+} else if (decisionChoice == 1 && computerChoice == 0) {
+    alert("You Win!");
+   
+} else if (decisionChoice == 0 && computerChoice == 2) {
+    ("You Win!");
+   
+} else if (decisionChoice == 2 && computerChoice == 1) {
+    alert("You Win!");
+   
+} else if (decisionChoice == 2 && computerChoice == 0) {
+    alert("You Loose!");
+    
+} else if (decisionChoice == 0 && computerChoice == 1) {
+    alert("You Loose!");
+    
+} else if (decisionChoice == 1 && computerChoice == 2) alert("You Loose!");
+
+}
+
+
+
 ;
